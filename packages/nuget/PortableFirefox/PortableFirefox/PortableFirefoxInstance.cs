4261ec1956fa4a63be01fda9a5544ca1433f7141
Miroljub <miroljub1995@gmail.com>
2023-08-14 15:07:58 +0200

    Update prop

--- packages/nuget/PortableFirefox/PortableFirefox/PortableFirefoxInstance.cs
+++ packages/nuget/PortableFirefox/PortableFirefox/PortableFirefoxInstance.cs
@@ -2,7 +2,7 @@ namespace PortableFirefox;
 
 public partial class PortableFirefoxInstance
 {
-    public string FirefoxPath => Path.Join(Path.Join(AppDomain.CurrentDomain.BaseDirectory,
+    public static string FirefoxPath => Path.Join(Path.Join(AppDomain.CurrentDomain.BaseDirectory,
         "portable-firefox-" + VERSION,
         "firefox-root"), "firefox");
 }
