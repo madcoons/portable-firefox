fd9ebb75670a42ccbcf9e994c611d3477b741efe
Miroljub <miroljub1995@gmail.com>
2023-08-15 23:11:25 +0200

    Update

--- packages/nuget/PortableFirefox/PortableFirefox.Test/PortableFirefoxTest.cs
+++ packages/nuget/PortableFirefox/PortableFirefox.Test/PortableFirefoxTest.cs
@@ -8,7 +8,7 @@ public class PortableFirefoxTest
     [Fact]
     public async Task Should_Return_Non_Null_Version()
     {
-        var res = await Cli.Wrap(Path.Join((ReadOnlySpan<char>) Path.Join((ReadOnlySpan<char>) AppDomain.CurrentDomain.BaseDirectory, (ReadOnlySpan<char>) "portable-firefox-116.0.2", (ReadOnlySpan<char>) "firefox-root"), (ReadOnlySpan<char>) "firefox-app", "firefox.run"))
+        var res = await Cli.Wrap(PortableFirefoxInstance.FirefoxPath)
             .WithArguments("--version")
             .WithValidation(CommandResultValidation.None)
             .ExecuteBufferedAsync();
