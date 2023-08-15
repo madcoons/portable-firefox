96d33132a20b32a2d1af5d795f590899b4694473
Miroljub <miroljub1995@gmail.com>
2023-08-15 23:10:28 +0200

    Update

--- packages/nuget/PortableFirefox/PortableFirefox/PortableFirefoxInstance.cs
+++ packages/nuget/PortableFirefox/PortableFirefox/PortableFirefoxInstance.cs
@@ -4,5 +4,5 @@ public partial class PortableFirefoxInstance
 {
     public static string FirefoxPath => Path.Join(Path.Join(AppDomain.CurrentDomain.BaseDirectory,
         "portable-firefox-" + VERSION,
-        "firefox-root"), "firefox");
+        "firefox-root"), "firefox-app", "firefox.run");
 }
