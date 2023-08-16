870910d29efe1505089f5d053a9ab5e4b5152dfc
Miroljub <miroljub1995@gmail.com>
2023-08-16 10:06:07 +0200

    Add GeckoDriver

--- packages/nuget/PortableFirefox/PortableFirefox/PortableFirefoxInstance.cs
+++ packages/nuget/PortableFirefox/PortableFirefox/PortableFirefoxInstance.cs
@@ -5,4 +5,8 @@ public partial class PortableFirefoxInstance
     public static string FirefoxPath => Path.Join(Path.Join(AppDomain.CurrentDomain.BaseDirectory,
         "portable-firefox-" + VERSION,
         "firefox-root"), "firefox-app", "firefox.run");
+    
+    public static string GeckoDriverPath => Path.Join(Path.Join(AppDomain.CurrentDomain.BaseDirectory,
+        "portable-firefox-" + VERSION,
+        "firefox-root"), "firefox-app", "geckodriver");
 }
