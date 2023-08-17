c7489b67831952c9d00d82155b17dc91ef8c1a55
Miroljub <miroljub1995@gmail.com>
2023-08-17 12:32:40 +0200

    Update versioning

--- packages/nuget/PortableFirefox/PortableFirefox/PortableFirefoxInstance.cs
+++ packages/nuget/PortableFirefox/PortableFirefox/PortableFirefoxInstance.cs
@@ -3,10 +3,10 @@ namespace PortableFirefox;
 public partial class PortableFirefoxInstance
 {
     public static string FirefoxPath => Path.Join(Path.Join(AppDomain.CurrentDomain.BaseDirectory,
-        "portable-firefox-" + VERSION,
+        "portable-firefox-" + VERSION + VERSION_SUFFIX,
         "firefox-root"), "firefox-app", "firefox.run");
     
     public static string GeckoDriverDir => Path.Join(Path.Join(AppDomain.CurrentDomain.BaseDirectory,
-        "portable-firefox-" + VERSION,
+        "portable-firefox-" + VERSION + VERSION_SUFFIX,
         "firefox-root"), "firefox-app");
 }
