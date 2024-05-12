034162191f43591f8d9bcff5deda7136300d5339
Miroljub <miroljub1995@gmail.com>
2024-05-12 14:39:06 +0200

    Update

--- BuildOSX.sh
+++ BuildOSX.sh
@@ -1,12 +1,26 @@
 #!/bin/sh
 set -e
 
-rm -rf tmp
+if [ -d "tmp" ]; then
+  rm -rf "tmp"
+fi
+mkdir tmp
+trap 'rm -rf tmp' EXIT
 
 curl -L "https://download.mozilla.org/?product=firefox-latest-ssl&os=osx&lang=en-US" --create-dirs --output-dir tmp --output "firefox.dmg"
 curl -L "https://github.com/mozilla/geckodriver/releases/download/v0.33.0/geckodriver-v0.33.0-macos.tar.gz" --create-dirs --output-dir tmp --output "geckodriver-macos.tar.gz"
 curl -L "https://github.com/mozilla/geckodriver/releases/download/v0.33.0/geckodriver-v0.33.0-macos-aarch64.tar.gz" --create-dirs --output-dir tmp --output "geckodriver-macos-aarch64.tar.gz"
 
+mkdir tmp/geckodriver-cross-arch
+tar -xvf "tmp/geckodriver-macos.tar.gz" -C tmp/geckodriver-cross-arch
+mv tmp/geckodriver-cross-arch/geckodriver tmp/geckodriver-cross-arch/geckodriver-x86_64
+tar -xvf "tmp/geckodriver-macos-aarch64.tar.gz" -C tmp/geckodriver-cross-arch
+mv tmp/geckodriver-cross-arch/geckodriver tmp/geckodriver-cross-arch/geckodriver-aarch64
+
 hdiutil attach -mountpoint tmp/app-mount tmp/firefox.dmg
 trap 'hdiutil detach tmp/app-mount' EXIT
-tar -zcvf tmp/firefox.tar.gz tmp/app-mount
+
+cp -r tmp/app-mount/Firefox.app tmp/
+
+lipo -create -output tmp/Firefox.app/Contents/MacOS/geckodriver tmp/geckodriver-cross-arch/geckodriver-x86_64 tmp/geckodriver-cross-arch/geckodriver-aarch64
+tar -zcvf tmp/firefox.tar.gz -C tmp Firefox.app
