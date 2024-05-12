ef8920713fea85f0e07d2be0cc134196955db63d
Miroljub <miroljub1995@gmail.com>
2024-05-12 17:21:45 +0200

    Update paths

--- packages/nuget/PortableFirefox/PortableFirefox/PortableFirefoxInstance.cs
+++ packages/nuget/PortableFirefox/PortableFirefox/PortableFirefoxInstance.cs
@@ -4,18 +4,24 @@ namespace PortableFirefox;
 
 public partial class PortableFirefoxInstance
 {
-    public static string FirefoxPath => Path.Join(Path.Join(AppDomain.CurrentDomain.BaseDirectory,
-        "portable-firefox-" + VERSION + VERSION_SUFFIX,
-        "firefox-root"), BinSubDir, "firefox.run");
+    public static string FirefoxPath => Path.Join(AbsoluteRootPath, BinSubDir, FirefoxBinName);
 
-    public static string GeckoDriverDir => Path.Join(Path.Join(AppDomain.CurrentDomain.BaseDirectory,
-        "portable-firefox-" + VERSION + VERSION_SUFFIX,
-        "firefox-root"), BinSubDir);
+    public static string GeckoDriverDir => Path.Join(AbsoluteRootPath, BinSubDir);
 
-    private static string BinSubDir =>
-        RuntimeInformation.IsOSPlatform(OSPlatform.Linux)
-            ? "firefox-app"
+    private static string AbsoluteRootPath
+        => Path.Join(AppDomain.CurrentDomain.BaseDirectory, "portable-firefox-" + VERSION + VERSION_SUFFIX);
+
+    private static string BinSubDir
+        => RuntimeInformation.IsOSPlatform(OSPlatform.Linux)
+            ? Path.Join("firefox-root", "firefox-app")
             : RuntimeInformation.IsOSPlatform(OSPlatform.OSX)
                 ? Path.Join("Firefox.app", "Contents", "MacOS")
+                : throw new("Unsupported platform.");
+
+    private static string FirefoxBinName
+        => RuntimeInformation.IsOSPlatform(OSPlatform.Linux)
+            ? "firefox.run"
+            : RuntimeInformation.IsOSPlatform(OSPlatform.OSX)
+                ? "firefox"
                 : throw new("Unsupported platform.");
 }
