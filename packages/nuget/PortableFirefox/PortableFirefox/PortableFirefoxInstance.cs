7b19bca0cce89bc586e17108a130eeccf23bf56f
Miroljub <miroljub1995@gmail.com>
2024-05-12 16:29:09 +0200

    Update paths

--- packages/nuget/PortableFirefox/PortableFirefox/PortableFirefoxInstance.cs
+++ packages/nuget/PortableFirefox/PortableFirefox/PortableFirefoxInstance.cs
@@ -1,12 +1,21 @@
+using System.Runtime.InteropServices;
+
 namespace PortableFirefox;
 
 public partial class PortableFirefoxInstance
 {
     public static string FirefoxPath => Path.Join(Path.Join(AppDomain.CurrentDomain.BaseDirectory,
         "portable-firefox-" + VERSION + VERSION_SUFFIX,
-        "firefox-root"), "firefox-app", "firefox.run");
-    
+        "firefox-root"), BinSubDir, "firefox.run");
+
     public static string GeckoDriverDir => Path.Join(Path.Join(AppDomain.CurrentDomain.BaseDirectory,
         "portable-firefox-" + VERSION + VERSION_SUFFIX,
-        "firefox-root"), "firefox-app");
+        "firefox-root"), BinSubDir);
+
+    private static string BinSubDir =>
+        RuntimeInformation.IsOSPlatform(OSPlatform.Linux)
+            ? "firefox-app"
+            : RuntimeInformation.IsOSPlatform(OSPlatform.OSX)
+                ? Path.Join("Firefox.app", "Contents", "MacOS")
+                : throw new("Unsupported platform.");
 }
